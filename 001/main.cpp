cb84f7489ac2190fd895a61d0100e071ec0d7c8f
Jorão Junior <jorgomjr@gmail.com>
2016-06-16 20:49:12 -0300

    Dicionario

--- 001/main.cpp
+++ 001/main.cpp
@@ -8,13 +8,14 @@ int main()
 
     /** COLOCAR EM UMA FUNCAO **/
   string line;
-  ifstream myfile ("/home/get01/Documentos/palavras.ispell.txt"); // ifstream = padrão ios:in
+  ifstream myfile ("/home/jorao/Documentos/trabalhoed2/001/Dicionarios/x.txt"); // ifstream = padrão ios:in
   if (myfile.is_open())
   {
     while (! myfile.eof() ) //enquanto end of file for false continua
     {
       getline (myfile,line); // como foi aberto em modo texto(padrão)
                              //e não binário(ios::bin) pega cada linha
+      if (line.compare("á") == 0)
       cout << line << endl;
     }
     myfile.close();
