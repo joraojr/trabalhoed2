31d1a44c08e5a5390217df839b8486fb87b35647
Jorão Junior <jorgomjr@gmail.com>
2016-07-07 20:55:32 -0300

    .

--- 001/include/No_Lista.h
+++ 001/include/No_Lista.h
@@ -5,17 +5,39 @@
 class No_RW;
 class No_Lista
 {
-    public:
-        No_Lista(){this->prox = NULL;};
-        ~No_Lista();
-        void setCaractereEspecial(char c){this->caractereEspecial = c;};
-        char getCaractereEspecial(){return this->caractereEspecial;};
-        void setProx(No_Lista* prox){ this->prox = prox;}
-        No_Lista* getProx(){return this->prox;};
-    private:
-        No_Lista* prox;
-        char caractereEspecial;
-        No_RW* filho;
+public:
+    No_Lista()
+    {
+        this->prox = NULL;
+    };
+    ~No_Lista();
+    void setCaractereEspecial(char c)
+    {
+        this->caractereEspecial = c;
+    };
+    char getCaractereEspecial()
+    {
+        return this->caractereEspecial;
+    };
+    void setProx(No_Lista* prox)
+    {
+        this->prox = prox;
+    }
+    No_Lista* getProx()
+    {
+        return this->prox;
+    };
+    No_RW* getFilho()
+    {
+        return this->filho;
+    };
+    void setFilho(No_RW* filho){
+        this->filho = filho;
+    };
+private:
+    No_Lista* prox;
+    char caractereEspecial;
+    No_RW* filho;
 };
 
 #endif // NO_LISTA_H
